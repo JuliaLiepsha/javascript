15756374f5b796ba3947997200613f1730d4ad40
JuliaLiepsha <lepsha2015@gmail.com>
2024-04-05 17:20:52 +0300

    update related to the range was added

--- ProbabilityTheory.js
+++ ProbabilityTheory.js
@@ -1,8 +1,10 @@
 let count = 10;
+let min = 100;
+let max = 1000;
 let number1 = number2 = 0;    
-    function checkProbabilityTheory(count) {     
+    function checkProbabilityTheory(count, min, max) {     
         for (let i = 1; i <= count; i++) {
-            z = Math.random() * 1000;
+            z = Math.random() * (max-min) + min;
             z = Math.round(z);
             console.log(z);   
             if (z % 2) {
@@ -13,10 +15,12 @@ let number1 = number2 = 0;
             }                
            }         
         }        
-checkProbabilityTheory(count);
+checkProbabilityTheory(count, min, max);
 procent2 = Math.round(number2 / (number2 + number1) *100);
 procent1 = 100-procent2;
 console.log(' Кількість згенерованих чисел:',count,'\n кількість парних чисел: ',number2,'\n кількість не парних чисел: ',number1,'\n співвідношення парних до не парних: ',procent2+'% / ',procent1+'%'); 
+
+
 
 
 
