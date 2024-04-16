cb0fa728a64e14bcd49d7c5155ec36412ecd1a41
JuliaLiepsha <lepsha2015@gmail.com>
2024-04-16 17:33:33 +0300

    updates added in sum of service costs.js

--- sum of service costs.js
+++ sum of service costs.js
@@ -2,39 +2,53 @@ var services = {
     "стрижка": "60 грн",
     "гоління": "80 грн",
     "Миття голови": "100 грн",
-    "Розбити скло": "200 грн"       
-    };
-    const values = Object.values(services); 
-    function  price(){
+    "Розбити скло": "200 грн",
+    price: function(){
+        values = Object.values(services); 
+        
         let sum = 0;
-        for (var i in values) {
-              values[i] = Number.parseFloat (values[i]);
-              sum = sum + values[i]                
-        }
-        console.log ("Сумма послуг", sum)
-    }
-
-    function  minPrice() {
-        let min = values[0]; 
-        for  (var i in values) {
-            values[i] = Number.parseFloat (values[i]);
-            if (values[i] < min) {
-                min = values[i]                
-            }            
-        }
-        console.log ("мінімальна вартість", min)
-    }
-
-    function  maxPrice() {
-            let max = values[0]; 
-        for  (var i in values) { 
-            values[i] = Number.parseFloat (values[i]);    
-            if (values[i] > max) {
-                max = values[i]                
+                for (let value of values) {
+                console.log(value);
+                if (typeof value != 'function') {
+                    sum += parseFloat(value);
+                }
+            
             }
+        console.log ("Сумма послуг", sum)
+
+    } ,
+    maxPrice: function(){
+        values = Object.values(services); 
+        let max = 0; 
+        for (let value of values) {
+            value = parseFloat(value);
+            if ((typeof value != 'function') && (value > max))
+             
+             {
+                max = value ;              
+             }
+        
         }
         console.log ("максимальна вартість",max)
+
+    },
+    minPrice: function(){
+        values = Object.values(services); 
+        let min = parseFloat(values[0]);; 
+        for (let value of values) {
+            value = parseFloat(value);
+            if ((typeof value != 'function') && (value < min))
+             
+             {
+                min = value ;              
+             }
+        
+        }
+        console.log ("мінімальна вартість",min)
+
+    }       
     };
-    price ();
-    minPrice ();
-    maxPrice(); 
+    services['new'] = '20 грн';
+    services.price();
+    services.maxPrice();
+    services.minPrice();
